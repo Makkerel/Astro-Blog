ebd60f8a5da6f794fed7c9d6911bc1c738676343
Kevin Zhao <kzhao132018@gmail.com>
2025-09-14 17:55:59 -0700

    removed animations. Added actual content. Added old blogs from previous website. Will continue to add content tommorrow. CSS will be for later.

--- src/scripts/menu.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const header = document.querySelector("header");
-
-  header.addEventListener("mouseleave", () => {
-    header.style.cssText = "";
-  });
-});
