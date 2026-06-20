5ab432813799422fc5388a7b19481fc031a662d2
Kevin Zhao <kzhao132018@gmail.com>
2026-06-19 21:38:18 -0700

    changed p font, added table of contnets, fixed some image bugs, add better image ux

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 import { unified } from "@astrojs/markdown-remark";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
@@ -7,6 +7,32 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+  fonts: [
+    {
+      name: "Share Tech",
+      cssVariable: "--font-share-tech",
+      provider: fontProviders.google(),
+      weights: [400],
+      styles: ["normal"],
+      subsets: ["latin"],
+    },
+    {
+      name: "Exo 2",
+      cssVariable: "--font-exo-2",
+      provider: fontProviders.google(),
+      weights: [700],
+      styles: ["normal"],
+      subsets: ["latin"],
+    },
+    {
+      name: "Atkinson Hyperlegible Next",
+      cssVariable: "--font-atkinson",
+      provider: fontProviders.google(),
+      weights: [400],
+      styles: ["normal"],
+      subsets: ["latin"],
+    },
+  ],
   markdown: {
     processor: unified({
       remarkPlugins: [remarkMath],
